eafb6f3d65911248b8e9391add818a792dfc4929
David Roman <david_roman09@yahoo.com>
2021-01-29 19:04:50 -0600

    html routes

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
+//HTML routes 
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+
 
 
 
