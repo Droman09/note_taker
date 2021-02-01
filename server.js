a3439bc11cfd7bf38f65bee19dd640e746e4411a
David Roman <david_roman09@yahoo.com>
2021-01-31 18:58:54 -0600

    working on API's

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var fs = require("fs");
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 
-//HTML routes 
-app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+
+    var notes = JSON.parse(data)
+    var noteId = 1
+
+    app.get("/api/notes", (req, res) => {
+        res.json(notes);
+        // console.log(notes)
+    })
+
+    app.post("/api/notes", (req, res) => {
+        req.body.id = parseInt(noteId)
+        notes.push(req.body);
+        res.json(notes)
+        update()
+        // console.log("success")
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+    })
+
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/notes.html"))
+    });
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/index.html"))
+    });
+
+    function update() {
+        fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), err => {
+            if (err) throw err;
+            return true;
+        })
+    }
 })
 
-app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
 
-
-
-
+//Server listen 
 app.listen(PORT, () => console.log("App listening on PORT: http://localhost:" + PORT));
